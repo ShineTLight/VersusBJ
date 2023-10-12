0ed2f977fe15f4cee8da9870a21049a5f693000f
Emre <shinethelight54@hotmail.com>
2023-10-13 00:06:19 +0300

    Deleted Goofy Debug Logs

--- Assets/GAME/Scripts/GameManager.cs
+++ Assets/GAME/Scripts/GameManager.cs
@@ -77,7 +77,6 @@ public class GameManager : MonoBehaviour
 
     public void StartPlayerTurn()
     {
-        Debug.Log("LOOOOOOOOOOOOOKKKKKKKKK MAAAAAANNNNNNNN: " + playerChoseToStay +" "+ aiChoseToStay);
         isPlayerTurn = true;
         drawCanvas.enabled = true;
         
